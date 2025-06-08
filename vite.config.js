8c2ae3dc19846314ac5b806660820cfb8817421f
Dayimshah <76784592+Dayimshah@users.noreply.github.com>
2025-06-08 12:52:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   base: './',
   plugins: [react()],
   build: {
-    outDir: 'build',  // change output folder name
-  },
+    outDir: 'build'  // or change this to 'dist' if you're using default
+  }
 })
